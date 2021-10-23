fe2babc6c5ba26a1cfbf32c8fa7fe9a004119cbd
Kizzz2911 <Kiejet22@gmail.com>
2021-10-23 09:02:53 +0700

    UpdateSplashScreen

---
+++ app/src/main/java/com/example/firebaseappchat/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.firebaseappchat
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent (this,Welcome::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
