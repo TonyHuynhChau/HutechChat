e4c372b584dd7342191bbf7d2c3c30b64d02e197
TonyHuynhChau <73379779+TonyHuynhChau@users.noreply.github.com>
2021-10-27 21:27:32 +0700

    Video Call XML

--- app/src/main/java/com/example/firebaseappchat/VideoCall/VideoChatActivity.kt
+++ app/src/main/java/com/example/firebaseappchat/VideoCall/VideoChatActivity.kt
@@ -15,29 +15,84 @@ import android.widget.FrameLayout;
 import java.util.*
 import android.content.Intent
 import android.net.Uri
+import android.os.Handler
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.firebaseappchat.UItem
+import com.example.firebaseappchat.messages.ChatLogActivity
+import com.example.firebaseappchat.registerlogin.SignUpActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.activity_video_chat.*
 
 
 class VideoChatActivity : AppCompatActivity() {
-    private val MY_PERMISSION_REQUEST_CODE_CALL_PHONE = 555
-    private val LOG_TAG = "AndroidExample"
-    private lateinit var editTextPhoneNum: EditText
-    private lateinit var buttoncall: Button
+
+    private lateinit var swipeCall: SwipeRefreshLayout
+    private lateinit var callRecyclerView: RecyclerView
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.firebaseappchat.R.layout.activity_video_chat)
-        this.editTextPhoneNum = findViewById<View>(com.example.firebaseappchat.R.id.editText1) as EditText
-        this.buttoncall = findViewById<View>(com.example.firebaseappchat.R.id.button1) as Button
+        this.swipeCall = findViewById<View>(com.example.firebaseappchat.R.id.swipeRefresh_call) as SwipeRefreshLayout
+        this.callRecyclerView = findViewById<View>(com.example.firebaseappchat.R.id.recyclerview_latest_messages_call) as RecyclerView
 
-        buttoncall.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                // TODO Auto-generated method stub
-                val i = Intent(Intent.ACTION_CALL)
-                i.data = Uri.parse("tel:" + editTextPhoneNum.getText().toString())
-                startActivity(i)
-            }
-        })
+        swipeCall.setOnRefreshListener {
+            Handler().postDelayed(Runnable {
+                swipeCall.isRefreshing = false
+            }, 4000)
+        }
+
+        LayUser()
     }
 
+    companion object {
+        var USER_KEY = "USER_KEY"
+    }
+
+    // Get user name and images then show in user_row
+    private fun LayUser() {
+        val userNguoiDung = FirebaseAuth.getInstance().currentUser
+        if (userNguoiDung != null) {
+            val Friends = FirebaseDatabase.getInstance().getReference("Friends")
+            Friends.child(userNguoiDung.uid).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val adapter = GroupAdapter<GroupieViewHolder>()
+                    snapshot.children.forEach() {
+                        val FriendUID = it.child("uid").value
+                        Log.d("FriendsUID", FriendUID.toString())
+                        val ref = FirebaseDatabase.getInstance().getReference("user")
+                        ref.addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                snapshot.children.forEach() {
+                                    val user = it.getValue(SignUpActivity.getUser::class.java)
+                                    if (user != null) {
+                                        if (userNguoiDung.uid != user.uid && FriendUID == user.uid) {
+                                            adapter.add(UItem(user))
+                                        }
+                                    }
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+                        })
+                    }
+                    recyclerview_latest_messages_call.adapter = adapter
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+        }
+    }
 
 }
