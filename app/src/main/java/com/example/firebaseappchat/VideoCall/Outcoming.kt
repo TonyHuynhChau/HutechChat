94e78f13381277263081b07f4e4401716853f1b0
TonyHuynhChau <73379779+TonyHuynhChau@users.noreply.github.com>
2021-10-30 14:11:32 +0700

    video call to firebase

--- app/src/main/java/com/example/firebaseappchat/VideoCall/Outcoming.kt
+++ app/src/main/java/com/example/firebaseappchat/VideoCall/Outcoming.kt
@@ -1,13 +1,184 @@
 package com.example.firebaseappchat.VideoCall
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
+import androidx.core.view.isVisible
+import com.example.firebaseappchat.NewMessActivity
 import com.example.firebaseappchat.R
+import com.example.firebaseappchat.registerlogin.LoginActivity
+import com.example.firebaseappchat.registerlogin.SignUpActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import fragment.HomeFragment
+import kotlinx.android.synthetic.main.user_row.view.*
+import kotlinx.android.synthetic.main.user_row_call.view.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class Outcoming : AppCompatActivity() {
+    private lateinit var username: TextView
+    private lateinit var btnCancell: ImageView
+    private lateinit var btnAccept: ImageView
+    private lateinit var avatar: CircleImageView
+
+    var toUser: SignUpActivity.getUser? = null
+    private lateinit var ocuRef: DatabaseReference
+
+    private lateinit var reciverUid: String
+
+    private lateinit var senderUid: String
+
+    private lateinit var callingUid: String
+
+    private lateinit var ringingUid: String
+
+    private lateinit var checker: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_outcoming)
+        val user: SignUpActivity.getUser
+        toUser = intent.getParcelableExtra(VideoChatActivity.USER_KEY)
+
+        reciverUid = intent.extras?.get(VideoChatActivity.USER_KEY).toString()
+        senderUid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+
+        username= findViewById(R.id.txtUsername_incomingCall)
+        avatar = findViewById(R.id.img_userIncomingCall)
+        btnCancell = findViewById(R.id.btn_rejectcall)
+        btnAccept = findViewById(R.id.btn_accpetcall)
+
+        checker = "clicked"
+
+        if (toUser != null) {
+            username.text = toUser!!.name
+            Picasso.get().load(toUser!!.Urlphoto).into(avatar)
+        }
+
+        btnCancell.setOnClickListener(){
+            checker = "clicked"
+            cancelCalling()
+        }
+    }
+
+    private fun cancelCalling() {
+        //Sender
+        ocuRef.child(senderUid).child("calling").addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists() && snapshot.hasChild("calling")) {
+                    callingUid = snapshot.child("calling").value.toString()
+
+                    ocuRef.child(callingUid).child("ringing").removeValue().addOnCompleteListener {
+                        if (it.isSuccessful){
+                            ocuRef.child(senderUid).child("calling").removeValue().addOnCompleteListener {
+                                startActivity(Intent(this@Outcoming, IncomingCall::class.java))
+                                finish()
+                            }
+                        }
+                    }
+                }
+                else{
+                    startActivity(Intent(this@Outcoming, HomeFragment::class.java))
+                    finish()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        //Reciveder
+        ocuRef.child(senderUid).child("ringing").addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists() && snapshot.hasChild("ringing")) {
+                    callingUid = snapshot.child("ringing").value.toString()
+
+                    ocuRef.child(ringingUid).child("calling").removeValue().addOnCompleteListener {
+                        if (it.isSuccessful){
+                            ocuRef.child(senderUid).child("calling").removeValue().addOnCompleteListener {
+                                startActivity(Intent(this@Outcoming, IncomingCall::class.java))
+                                finish()
+                            }
+                        }
+                    }
+                }
+                else{
+                    startActivity(Intent(this@Outcoming, HomeFragment::class.java))
+                    finish()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val fromId = FirebaseAuth.getInstance().uid
+        val toId = toUser?.uid
+
+        //Call Event
+        ocuRef = FirebaseDatabase.getInstance().getReference("/user-call/$fromId/$toId")
+        ocuRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(!checker.equals("clicked") && !snapshot.hasChild("calling") && !snapshot.hasChild("ringing")){
+                    var callingInfo: HashMap<String, Any> = HashMap()
+
+                    callingInfo.put("calling", reciverUid)
+
+                        ocuRef.child(senderUid).child("calling")
+                        .updateChildren(callingInfo as Map<String, Any>)
+                        .addOnCompleteListener {
+                            fun onComplete(@NonNull task: Task<Void>) {
+                                if(task.isSuccessful()){
+                                    var ringingInfo: HashMap<String, Any> = HashMap()
+
+                                    ringingInfo.put("ringing", senderUid)
+
+                                    ocuRef.child(reciverUid).child("ringing")
+                                        .updateChildren(ringingInfo as Map<String, Any>)
+                                }
+                            }
+                        }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        //Cancal button
+        ocuRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child(senderUid).hasChild("ringing") && snapshot.child(senderUid).hasChild("calling")){
+                    btnAccept.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
+
--- app/src/main/java/com/example/firebaseappchat/VideoCall/VideoChatActivity.kt
+++ app/src/main/java/com/example/firebaseappchat/VideoCall/VideoChatActivity.kt
@@ -9,47 +9,111 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.view.SurfaceView;
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.FrameLayout;
 import java.util.*
 import android.content.Intent
 import android.net.Uri
 import android.os.Handler
 import android.util.Log
+import android.widget.*
+import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.firebaseappchat.NewMessActivity
 import com.example.firebaseappchat.UItem
 import com.example.firebaseappchat.messages.ChatLogActivity
+import com.example.firebaseappchat.messages.MainActivity
+import com.example.firebaseappchat.registerlogin.LoginActivity
 import com.example.firebaseappchat.registerlogin.SignUpActivity
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import de.hdodenhof.circleimageview.CircleImageView
+import fragment.HomeFragment
 import kotlinx.android.synthetic.main.activity_video_chat.*
+import kotlinx.android.synthetic.main.user_row.view.*
+import kotlinx.android.synthetic.main.user_row_call.*
+import kotlinx.android.synthetic.main.user_row_call.view.*
+import kotlin.jvm.internal.Ref
 
 
 class VideoChatActivity : AppCompatActivity() {
 
     private lateinit var swipeCall: SwipeRefreshLayout
-    private lateinit var callRecyclerView: RecyclerView
-    
+    private lateinit var currentUserId: String
+    private lateinit var ref: DatabaseReference
+    private lateinit var calledBy: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.firebaseappchat.R.layout.activity_video_chat)
-        this.swipeCall = findViewById<View>(com.example.firebaseappchat.R.id.swipeRefresh_call) as SwipeRefreshLayout
-        this.callRecyclerView = findViewById<View>(com.example.firebaseappchat.R.id.recyclerview_latest_messages_call) as RecyclerView
+        this.swipeCall = findViewById<View>(R.id.swipeRefresh_call) as SwipeRefreshLayout
 
+        currentUserId = FirebaseAuth.getInstance().currentUser!!.uid
         swipeCall.setOnRefreshListener {
             Handler().postDelayed(Runnable {
                 swipeCall.isRefreshing = false
             }, 4000)
         }
-
+        //Load Friend User
+        verifyUserLoggedIn()
         LayUser()
+
+        //Call Event
+        validateUser()
+        checkForReceivingCall()
+    }
+
+    private fun checkForReceivingCall() {
+        ref.child(currentUserId).child("ringing")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.hasChild("ringing")) {
+                        calledBy = snapshot.child("ringing").getValue().toString()
+                        val intent = Intent(this@VideoChatActivity, Outcoming::class.java)
+                        intent.putExtra(USER_KEY, calledBy)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
+    private fun validateUser() {
+        ref = FirebaseDatabase.getInstance().getReference()
+
+        ref.child("user").child(currentUserId).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // TODO: 10/28/2021
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                val intent = Intent(this@VideoChatActivity, VideoChatActivity::class.java)
+                startActivity(intent)
+                finish()
+                Toast.makeText(this@VideoChatActivity, "Gọi Thất Bại", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+
+    private fun verifyUserLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 
     companion object {
@@ -74,14 +138,18 @@ class VideoChatActivity : AppCompatActivity() {
                                     val user = it.getValue(SignUpActivity.getUser::class.java)
                                     if (user != null) {
                                         if (userNguoiDung.uid != user.uid && FriendUID == user.uid) {
-                                            adapter.add(UItem(user))
+                                            adapter.add(CallItem(user))
                                         }
                                     }
                                 }
                             }
 
                             override fun onCancelled(error: DatabaseError) {
-                                TODO("Not yet implemented")
+                                Toast.makeText(
+                                    this@VideoChatActivity,
+                                    "Gọi Thất Bại",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         })
                     }
@@ -95,4 +163,35 @@ class VideoChatActivity : AppCompatActivity() {
         }
     }
 
+    //Connect user_row to recycler view
+    class CallItem(val user: SignUpActivity.getUser) : Item<GroupieViewHolder>() {
+        override fun getLayout(): Int {
+            return R.layout.user_row_call
+        }
+
+        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+            if (user.equals("null")) {
+                Log.d("Error Call", "User Name = ${user.name}")
+                return
+            } else if (user.Urlphoto.isEmpty()) {
+                val ImgDefault = "https://th.bing.com/th/id/R.502a73beb3f9263ca076457d525087c6?" +
+                        "rik=OP8RShVgw6uFhQ&riu=http%3a%2f%2fdvdn247.net%2fwp-content%2fuploads%2f2020%2f07%2" +
+                        "favatar-mac-dinh-1.png&ehk=NSFqDdL3jl9cMF3B9A4%2bzgaZX3sddpix%2bp7R%2bmTZHsQ%3d&risl=" +
+                        "&pid=ImgRaw&r=0"
+                viewHolder.itemView.TxtUserName_call.text = user.name + "(Need Update Profile)"
+                Picasso.get().load(ImgDefault).into(viewHolder.itemView.iVUser_call)
+            } else {
+                viewHolder.itemView.TxtUserName_call.text = user.name
+                Picasso.get().load(user.Urlphoto).into(viewHolder.itemView.iVUser_call)
+            }
+            viewHolder.itemView.btn_VideoCall.setOnClickListener() {
+                val intent = Intent(viewHolder.itemView.context, Outcoming::class.java)
+                intent.putExtra(USER_KEY, user)
+                viewHolder.itemView.context.startActivity(intent)
+            }
+        }
+    }
 }
+
+
+
