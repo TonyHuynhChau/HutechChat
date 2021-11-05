0ac8e65d00bd551774eb62e417fe9b8bfaff1a3c
TonyHuynhChau <73379779+TonyHuynhChau@users.noreply.github.com>
2021-11-05 12:06:14 +0700

    multi languages pre and delete unused thing

--- app/src/main/java/com/example/firebaseappchat/Welcome.kt
+++ app/src/main/java/com/example/firebaseappchat/Welcome.kt
@@ -2,21 +2,17 @@ package com.example.firebaseappchat
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebaseappchat.databinding.ActivityWelcomeBinding
 import com.example.firebaseappchat.registerlogin.LoginActivity
 import com.example.firebaseappchat.registerlogin.SignUpActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
+
 
 class Welcome : AppCompatActivity() {
     private lateinit var data : FirebaseAuth
     private lateinit var binding : ActivityWelcomeBinding
 
-    var selectIndex = 0
-    val langNames = arrayOf("English","Vietnamese","Japanese")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +25,6 @@ class Welcome : AppCompatActivity() {
         binding.BtnDangNhap.setOnClickListener{
             btnDN()
         }
-
-
-    }
-
-    fun selectItem(view: View){
-        var selectItem = langNames[selectIndex]
-        val langDialog = MaterialAlertDialogBuilder(this)
-        langDialog.setTitle("App Languages")
-        langDialog.setSingleChoiceItems(langNames,selectIndex){
-            dialog,which ->
-            selectIndex = which
-            selectItem = langNames[which]
-        }
-        langDialog.setPositiveButton("Ok"){
-            dialog,which ->
-            Toast.makeText(this,"$selectItem has selected",Toast.LENGTH_SHORT).show()
-        }
-        langDialog.setNeutralButton("Cancel"){
-            dialog,which ->
-            dialog.dismiss()
-        }
-        langDialog.show()
     }
 
     private fun btnDN() {
