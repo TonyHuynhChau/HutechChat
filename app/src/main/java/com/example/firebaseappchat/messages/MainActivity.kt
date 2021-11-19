c936f7affff6c402b66845bfc59d9cc06dd125a6
matri001 <63767292+matri001@users.noreply.github.com>
2021-11-19 17:05:52 +0700

    menu_dangxuat

--- app/src/main/java/com/example/firebaseappchat/messages/MainActivity.kt
+++ app/src/main/java/com/example/firebaseappchat/messages/MainActivity.kt
@@ -136,6 +136,12 @@ class MainActivity : AppCompatActivity() {
             R.id.menu_new_call -> {
                 startActivity(Intent(this, VideoChatActivity::class.java))
             }
+            R.id.menu_sign_out ->{
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
         }
 
         return super.onOptionsItemSelected(item)
